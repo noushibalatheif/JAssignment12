9e8184346acbe27dea40b79cbcb4dec5bd0eafa1
noushibalatheif <noushibalatheif@gmail.com>
2017-07-06 10:55:32 -0700

    Java Asgmnt 12

---
+++ main.java
@@ -0,0 +1,15 @@
+package assignment12;
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		contractEmployee CE=new contractEmployee
+				("Mary", 24, 1002, "Female", 9823672, 100, 100, 10200);
+		CE.setDuration(6);
+		CE.getDuration();
+		CE.display();
+		CE.disp();
+	}
+	
+}
